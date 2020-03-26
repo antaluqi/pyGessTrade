db6ab66b2e6d7f83c51d2b85186aba0ab86aba43
antaluqi <antaluqi@163.com>
2020-03-27 00:37:12 +0800

    多线程及界面初步

--- App.py
+++ App.py
@@ -1,12 +1,60 @@
+from PyQt5.QtCore import QThread ,  pyqtSignal,  QDateTime 
+from PyQt5.QtWidgets import QApplication,  QDialog,  QLineEdit
+import time
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget  #导入必要的组件
+import api.GressTrade as gt
+
+# 价格获取线程
+class GetQuoteThread(QThread):
+    update_signal=pyqtSignal(str)
+
+    def run(self):
+        api=gt.API()
+        islog,logMasg=api.login('1021805322','615919')
+        print(logMasg)
+        while True:
+            api.getQuote2()
+            s=api.quote.mautd.quoteTime+':  '+str(api.quote.mautd.last)
+            self.update_signal.emit(s)
+# 显示刷新线程
+class DisplayThread(QThread):
+    flash_signal=pyqtSignal()
+
+    def run(self):
+        while True:
+            self.flash_signal.emit()
+            time.sleep(1)
+
+class Window(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle('PyQt 5界面实时更新例子')
+        self.resize(400, 100)
+        self.input = QLineEdit(self)
+        self.input.resize(400, 100)
+        self.initUI()
+        self.quoteStr='价格'
+    def initUI(self):
+        # 创建线程
+        self.getquote = GetQuoteThread()
+        self.display = DisplayThread()
+
+        # 连接信号
+        self.getquote.update_signal.connect(self.valueChange)
+        self.display.flash_signal.connect(self.flashInput)
+        # 开始线程
+        self.getquote.start()
+        self.display.start()
+
+    def valueChange(self,data):
+        self.quoteStr=data
+    
+    def flashInput(self):
+        self.input.setText(self.quoteStr)
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)   #每一个程序都要建立一个，相当于一个画布，其他组件可以在上面画，放置，是一个父组件 sys.argv用来表示可以用命令行运行代码
-    w = QWidget()       #建立一个窗口
-    w.resize(250, 150)  #重置为大小250，150大小的窗口
-    w.move(300, 300)    #移动到300，300这个桌面的坐标上
-    w.setWindowTitle('Simple') #设置窗口的标题
-    w.show()            #显示它，如果没有这句话，前面做的工作就没法看到，只是存进了内存里
-    sys.exit(app.exec_()) #关闭程序，没有这句就只能强制关闭程序了，app.exec_()，只有这个好像也可以成功，可能是需要让程序知道进程都已经释放了吧
-
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show() 
+    sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,40 +1,43 @@
-import api.GressTrade as gt
-from multiprocessing import Process,Queue
-import multiprocessing
-import os
-import pprint
+from PyQt5.QtCore import QThread ,  pyqtSignal,  QDateTime 
+from PyQt5.QtWidgets import QApplication,  QDialog,  QLineEdit
 import time
+import sys
 
+class BackendThread(QThread):
+    # 通过类成员对象定义信号
+    update_date = pyqtSignal(str)
 
+    # 处理业务逻辑
+    def run(self):
+        while True:
+            data = QDateTime.currentDateTime()
+            currTime = data.toString("yyyy-MM-dd hh:mm:ss")
+            self.update_date.emit( str(currTime) )
+            time.sleep(1)
 
-def recvQuote(q,api):
-    for i in range(2000):
-        api.getQuote2()
-        q.put(api.quote.mautd.last)
-        #q.put(i)
-        print('进程1执行%d'%(i))
-        #print(q.qsize())
+class Window(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle('PyQt 5界面实时更新例子')
+        self.resize(400, 100)
+        self.input = QLineEdit(self)
+        self.input.resize(400, 100)
+        self.initUI()
 
-def printQuote(q,api):
-    for i in range(2000):
-        price=q.get()
-        print('进程2执行--%f'%(price))
-        
+    def initUI(self):
+        # 创建线程
+        self.backend = BackendThread()
+        # 连接信号
+        self.backend.update_date.connect(self.handleDisplay)
+        # 开始线程
+        self.backend.start()
 
-if __name__=='__main__':
-    api=gt.API()
-    islog,logMasg=api.login('1021805322','615919')
-    print(logMasg)
+    # 将当前时间输出到文本框
+    def handleDisplay(self, data):
+        self.input.setText(data)
 
-    q=Queue(1)
-    p1 = Process(target=recvQuote, args=(q,api,))
-    p2 = Process(target=printQuote, args=(q,api,))
-    p2.daemon=True
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
-    api.Close()
-    
-
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show() 
+    sys.exit(app.exec_())
